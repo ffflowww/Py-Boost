c3e48dceadb8b376fbb073a62c66a8f5c037c7f7
f10w <m8flow@gmail.com>
2022-11-23 11:09:18 +0300

    done2

--- tests/main_test.py
+++ tests/main_test.py
@@ -19,7 +19,7 @@ def test_reg(target_splitter, batch_size, pc=False):
         X, y = X[-50000:], y[-50000:]
     else:
         X_test, y_test = X[:192000], y[:192000]
-        trees = 20
+        trees = 200
         X, y = X[-50000:], y[-50000:]
     model = GradientBoosting('mse', 'r2_score',
                              ntrees=trees, lr=.01, verbose=5, es=200, lambda_l2=1,
@@ -54,13 +54,13 @@ def test_reg(target_splitter, batch_size, pc=False):
 if __name__ == '__main__':
     print(f"Start tests with cuda: {cp.cuda.runtime.runtimeGetVersion()}")
     print(os.environ['CONDA_DEFAULT_ENV'])
-    # pc = True
-    pc = False
+    pc = True
+    # pc = False
 
 
     with nvtx.annotate("Test case 1"):
         if pc:
-            test_reg("OneVsAll", batch_size=4444, pc=pc)
+            test_reg("OneVsAll", batch_size=3333, pc=pc)
         else:
             test_reg("OneVsAll", batch_size=3333, pc=pc)
             # test_reg("Single", batch_size=8000, pc=pc)
