3d04a0b2bce47b9038764a9ebe3411b3a3ca80d0
f10w <m8flow@gmail.com>
2022-12-28 08:57:51 +0300

    changing 1

--- tests/main_test.py
+++ tests/main_test.py
@@ -77,8 +77,8 @@ def test_reg(target_splitter, batch_size, params):
     stages = [5, 15, 20, 21, 44, 49]
     # stages = [49]
 
-    ps_orig = model.predict_staged_deprecated(X_test, iterations=stages, batch_size=1000)
-    ps_new = model.predict_staged(X_test, iterations=stages, batch_size=1000)
+    ps_orig = model.predict_staged_deprecated(X_test, iterations=stages, batch_size=10000)
+    ps_new = model.predict_staged(X_test, iterations=stages, batch_size=10000)
 
     # for i in range(len(stages)):
     #     print(ps_orig[i][1])
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     # }
 
     params = {
-        "x_size": 5500,
+        "x_size": 55000,
         "feat_size": 20,
         "y_size": 6,
         "n_trees": 50
