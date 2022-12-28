81083efb215f0708cb3ef6dfe647d507576df637
f10w <m8flow@gmail.com>
2022-12-28 03:53:10 +0300

    changing 1

--- tests/main_test.py
+++ tests/main_test.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
         "x_size": 1050000,
         "feat_size": 50,
         "y_size": 16,
-        "n_trees": 100
+        "n_trees": 200
     }
 
     with nvtx.annotate("Test case 1, OneVsAll"):
