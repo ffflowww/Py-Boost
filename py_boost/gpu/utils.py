5f8af3424a472caa4f31b92d8978a607cd977add
f10w <m8flow@gmail.com>
2022-12-30 08:46:19 +0300

    fixes v1

--- py_boost/gpu/utils.py
+++ py_boost/gpu/utils.py
@@ -633,9 +633,9 @@ tree_prediction_kernel_old_format = cp.RawKernel(
         int val_offset = n_out * leaves[node * n_groups + j_];
         for(int out_i = 0; out_i < n_out; ++out_i) {
             if (out_indexes[out_i] == j_) {
-                res[res_offset + out_i] = values[val_offset + out_i];
+                res[res_offset + out_i] += values[val_offset + out_i];
             }
-        }
+        }ы
     }
     ''',
     'tree_prediction_kernel_old_format')
