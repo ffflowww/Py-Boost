8aa2597db191a214b61b32f7f421cbf8d0083046
f10w <m8flow@gmail.com>
2023-01-18 10:40:12 +0300

    fixes

--- tests/main_test.py
+++ tests/main_test.py
@@ -12,21 +12,21 @@ from py_boost.gpu.inference import EnsembleInference
 def test_reg(target_splitter, batch_size, params):
     X, y = make_regression(params["x_size"], params["feat_size"], n_targets=params["y_size"], random_state=42)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.95, random_state=42)
-    # model = GradientBoosting('mse', 'r2_score',
-    #                          ntrees=params["n_trees"], lr=.01, verbose=20, es=200, lambda_l2=1,
-    #                          subsample=.8, colsample=.8, min_data_in_leaf=10, min_gain_to_split=0,
-    #                          max_bin=256, max_depth=6, target_splitter=target_splitter,
-    #                          debug=False)
-    # model.fit(X, y, eval_sets=[{'X': X_test, 'y': y_test},])
+    model = GradientBoosting('mse', 'r2_score',
+                             ntrees=params["n_trees"], lr=.01, verbose=20, es=200, lambda_l2=1,
+                             subsample=.8, colsample=.8, min_data_in_leaf=10, min_gain_to_split=0,
+                             max_bin=256, max_depth=6, target_splitter=target_splitter,
+                             debug=False)
+    model.fit(X, y, eval_sets=[{'X': X_test, 'y': y_test},])
 
-    print(X_test)
+    # print(X_test)
     # X_test.ravel()[np.random.choice(X_test.size, X_test.size // 10, replace=False)] = np.nan
 
     X_test[np.random.rand(X_test.shape[0], X_test.shape[1]) > 0.5] = np.nan
     # X_test[...] = np.nan
-    print(X_test)
+    # print(X_test)
 
-    exit()
+    # exit()
 
     print("Testing orig prob...")
     with nvtx.annotate("pred orig prob"):
