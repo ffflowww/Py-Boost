763c34a0ce9ed4b90acde113bbb149955ba27441
f10w <m8flow@gmail.com>
2023-01-20 11:39:34 +0300

    fixes

--- py_boost/gpu/tree.py
+++ py_boost/gpu/tree.py
@@ -302,7 +302,7 @@ class Tree:
             q = [(0, 0)]
             while len(q) != 0:  # BFS
                 n_old, n_new = q[0]
-                if self.nans[i][n_old] is False:
+                if self.nans[i][n_old] is True:
                     nf[4 * (gr_subtree_offsets[i] + n_new)] = float(self.feats[i][n_old] + 1)
                 else:
                     nf[4 * (gr_subtree_offsets[i] + n_new)] = float(-(self.feats[i][n_old] + 1))
@@ -332,7 +332,12 @@ class Tree:
         print(nf)
         print("???")
         print("NANS")
+        print(self.nans.shape)
         print(self.nans)
+        print("end")
+        print("feats")
+        print(self.feats.shape)
+        print(self.feats)
         print("end")
         # print("AFTER!!!")
         # c = cp.array(nf, dtype=cp.float32)
