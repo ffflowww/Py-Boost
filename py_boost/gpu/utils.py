2f9af5191e612480f87ca7bf2c7a27b89065c6e1
f10w <m8flow@gmail.com>
2023-02-27 03:36:41 +0300

    bruh

--- py_boost/gpu/utils.py
+++ py_boost/gpu/utils.py
@@ -759,7 +759,7 @@ tree_prediction_kernel_alltogether2 = cp.RawKernel(
                 //long long index_offset = n_out * i_m;
                 //for(; i_out < i_out_end; ++i_out) {
                 //    res[i_out_offset + gr_out_indexes[index_offset + i_out]] += values[value_offset + gr_out_indexes[index_offset + i_out]];
-                }
+                //}
             }
         }
         
